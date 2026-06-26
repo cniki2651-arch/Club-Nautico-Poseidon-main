b31039992ce6128d22d7c120e90e0cdc27d917f7
cniki2651-arch <cniki2651@gmail.com>
2026-06-25 23:14:27 -0500

    modificació en el login

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -192,6 +192,7 @@ export default function LoginPage() {
             
             <p className="text-center text-[10px] text-slate-400 mt-8 leading-tight">
               Acceso restringido a personal autorizado.
+              © 2026 Club Poseidón
             </p>
           </div>
         </div>
